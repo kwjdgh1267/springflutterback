7c1a4caea1d81b5b55ab5ea1b2c99b2187b839e4
kwjdgh1267 <78252310+kwjdgh1267@users.noreply.github.com>
2024-06-13 08:20:56 +0900

    Update ExerciseService.java
    주석 추가

--- src/main/java/com/example/flutterconnenttest/service/ExerciseService.java
+++ src/main/java/com/example/flutterconnenttest/service/ExerciseService.java
@@ -18,9 +18,9 @@ public class ExerciseService {
     public ResponseEntity<ExerciseDto> saveExercise(String title,String type) {
         Exercise savedExercise = exerciseRepository.save(new Exercise(title,type));
         ExerciseDto exerciseDto= new ExerciseDto(savedExercise.getId(), savedExercise.getTitle(), savedExercise.getType());
-        if(savedExercise==null){
+        if(savedExercise==null){//저장이 안됐다면 에러
             return ResponseEntity.badRequest().build();
-        }else{
+        }else{//저장이 됐다면 200 반환
             return ResponseEntity.ok().body(exerciseDto);
         }
     }
@@ -31,7 +31,7 @@ public class ExerciseService {
         return ResponseEntity.ok().body(dtos);
     }
     public ResponseEntity<List<ExerciseDto>> showExerciseByType(String type) {
-        List<Exercise> exercises = exerciseRepository.findAllByTypeOrTitleContaining(type,type);
+        List<Exercise> exercises = exerciseRepository.findAllByTypeOrTitleContaining(type,type);//운동 종류나 이름에 특정 키워드가 들어가는것 반환
         List<ExerciseDto> dtos =exercises.stream()
                 .map((exercise)->new ExerciseDto(exercise.getId(), exercise.getTitle(), exercise.getType())).collect(Collectors.toList());
         return ResponseEntity.ok().body(dtos);
