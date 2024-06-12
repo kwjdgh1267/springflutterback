78561f5d3913a907ed4fca02572ad3daa014c77c
kwjdgh1267 <78252310+kwjdgh1267@users.noreply.github.com>
2024-06-13 08:13:40 +0900

    Update ExerciseRepository.java
    주석 추가

--- src/main/java/com/example/flutterconnenttest/repository/ExerciseRepository.java
+++ src/main/java/com/example/flutterconnenttest/repository/ExerciseRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise,Long> {
-    Exercise findByTitle(String title);
-    List<Exercise> findAllByTypeOrTitleContaining(String type,String title);
+    Exercise findByTitle(String title); //운동 이름으로 검색. 이름중에 특정 키워드가 들어가는 운동들 반환
+    List<Exercise> findAllByTypeOrTitleContaining(String type,String title);// 이름이나 어느 부위 운동인지가 검색조건임
 }
