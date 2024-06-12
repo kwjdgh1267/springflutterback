37ff69aa30315881b38947643d127d873f220267
kwjdgh1267 <78252310+kwjdgh1267@users.noreply.github.com>
2024-06-13 08:18:54 +0900

    Update RecordRepository.java
    주석 추개

--- src/main/java/com/example/flutterconnenttest/repository/RecordRepository.java
+++ src/main/java/com/example/flutterconnenttest/repository/RecordRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecordRepository extends JpaRepository<Record,Long> {
+    //달력에서 선택한 날짜에 해당하는 운동 기록들을 가져옴
     List<Record> findAllByYearAndMonthAndDate(int year, int month, int date);
 
 }
