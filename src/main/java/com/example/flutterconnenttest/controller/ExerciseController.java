78a75607ca6ff18916368c59a8ee087999c2e5c2
kwjdgh1267 <78252310+kwjdgh1267@users.noreply.github.com>
2024-06-13 08:06:52 +0900

    Update ExerciseController.java
    주석 추가

--- src/main/java/com/example/flutterconnenttest/controller/ExerciseController.java
+++ src/main/java/com/example/flutterconnenttest/controller/ExerciseController.java
@@ -12,15 +12,15 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
-public class ExerciseController {//운동 엔티티에 어디 운동인지 추가해야함
+public class ExerciseController {//운동과 관련된 요청을 처리하는 컨트롤러
     private final ExerciseService exerciseService;
 
-    @PostMapping("/exercise")
+    @PostMapping("/exercise")//운동 저장 기능으로 클라이언트에게는 공개되지 않음
     public ResponseEntity<ExerciseDto> saveExercise(@RequestParam(name = "title") String title, @RequestParam(name = "type") String type){
         return exerciseService.saveExercise(title,type);
     }
 
-    @GetMapping("/exercise")
+    @GetMapping("/exercise")//검색 조건이 없다면 모두 조회, 있다면 해당 조건에 운동만 보여주는 기능
     public ResponseEntity<List<ExerciseDto>> showExercise(@RequestParam(name = "type",required = false)String type){
         if(type != null){
             return exerciseService.showExerciseByType(type);
